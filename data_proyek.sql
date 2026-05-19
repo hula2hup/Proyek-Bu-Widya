14b090ba2b708c94638a4557e145259389e82c21
briannugraha <brian.nugraha@ui.ac.id>
2026-05-19 14:57:07 +0700

    database

--- data_proyek.sql
+++ data_proyek.sql
@@ -1,8 +1,14 @@
-CREATE TABLE data_proyek (
-    id VARCHAR(50) PRIMARY KEY,
-    timestamp DATETIME,
-    nama_proyek VARCHAR(255),
-    lokasi VARCHAR(255)
-    -- Tambahkan kolom lain di sini sesuai kebutuhan HTML kamu
-);
+-- 1. Membuat database baru jika belum ada
+CREATE DATABASE IF NOT EXISTS db_data_proyek;
 
+-- 2. Menggunakan database yang baru dibuat
+USE db_data_proyek;
+
+-- 3. Membuat tabel 'data_proyek' sesuai kebutuhan form HTML & PHP
+CREATE TABLE IF NOT EXISTS data_proyek (
+    id VARCHAR(50) NOT NULL,
+    timestamp DATETIME NOT NULL,
+    nama_proyek VARCHAR(255) DEFAULT NULL,
+    lokasi VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
